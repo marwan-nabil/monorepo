1b7755958965e74f2c12f95cfa05f18273beca97
Marwan Nabil <marwan.nabil.mohammed@gmail.com>
2023-12-05 16:00:29 +0200

    move tools each to a separate folder

--- scripts/bootstrap.bat
+++ scripts/bootstrap.bat
@@ -6,13 +6,13 @@ if "%1"=="debug" (
     set cc_flags=/Od /Z7
 )
 
-pushd ..\tools
+pushd ..\tools\build
     cl^
         /nologo %cc_flags% /Oi /FC /GR- /EHa-^
         /W4 /WX /wd4201 /wd4100 /wd4189 /wd4505 /wd4456 /wd4996 /wd4018^
         /D_CRT_SECURE_NO_WARNINGS /D_CRT_RAND_S /DENABLE_ASSERTIONS^
-        /I..\^
-        ..\projects\tools\build\build.cpp^
+        /I..\..\^
+        ..\..\projects\tools\build\build.cpp^
         /link /subsystem:console /incremental:no /opt:ref user32.lib shell32.lib
 
     del build.obj
--- shell_startup.bat
+++ shell_startup.bat
@@ -1,22 +1,23 @@
 @echo off
 
-@REM adding gcc cross compiler, binutils & dependencies to path
+@REM adding gcc cross compiler, binutils & dependencies to shell path
 set path=%cd%\tools\cygwin\lib\gcc\x86_64-pc-cygwin\11;%path%
 set path=%cd%\tools\cygwin\bin;%path%
 set path=%cd%\tools\i686-elf\bin;%path%
 
-@REM adding external tools to path
+@REM adding external tools to shell path
 set path=%cd%\tools\nasm;%path%
 set path=%cd%\tools\qemu;%path%
 set path=%cd%\tools\bochs;%path%
 
-@REM add local scripts and tools to system path
+@REM add local scripts and tools to shell path
+set path=%cd%\tools\build;%path%
+set path=%cd%\tools\lint;%path%
 set path=%cd%\scripts;%path%
-set path=%cd%\tools;%path%
 
 @REM setup MSVC environment
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 if not exist output; mkdir output
-if not exist tools; mkdir tools
+if not exist tools\build; mkdir tools\build
 cd output
